d7f4bc7fd6c948392a99de93595f070bb051f763
rrgmon <rrgmon@protonmail.com>
2023-09-07 21:08:21 -0400

    Update user.lua

--- plugins/user.lua
+++ plugins/user.lua
@@ -13,8 +13,6 @@ return {
   "AstroNvim/astrocommunity",
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.pack.rust" },
-  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.pack.java" },
 }
